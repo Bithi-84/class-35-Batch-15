a131bd101381aa98d5605796ed99708669865205
Bithi-84 <tehzeebbithi.2016@gmail.com>
2024-10-31 15:26:06 +0600

    js DOM

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function grade()
+{
+
+    let marks = document.getElementById('marks').value;
+
+    if(parseFloat(marks>=80))
+
+    {
+         document.getElementById('result').innerHTML = "The grade is=A+"
+    }
+       
+
+}
