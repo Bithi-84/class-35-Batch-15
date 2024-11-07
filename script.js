d4d1e27895fdbbfdf41008b7dc4f88081f2552dc
Bithi-84 <tehzeebbithi.2016@gmail.com>
2024-11-07 14:52:55 +0600

    problem solve

--- script.js
+++ script.js
@@ -4,11 +4,45 @@ function grade()
 
     let marks = document.getElementById('marks').value;
 
-    if(parseFloat(marks>=80))
+    if(parseFloat(marks)>=80)
 
     {
          document.getElementById('result').innerHTML = "The grade is=A+"
     }
-       
 
+   else if(parseFloat(marks)>=70)
+
+        {
+             document.getElementById('result').innerHTML = "The grade is=A"
+        }
+       
+      else if(parseFloat(marks)>=60)
+
+            {
+                 document.getElementById('result').innerHTML = "The grade is=A-"
+            }
+
+            else if(parseFloat(marks)>=50)
+
+                {
+                     document.getElementById('result').innerHTML = "The grade is=B"
+                }
+            
+                else if(parseFloat(marks)>=40)
+
+                    {
+                         document.getElementById('result').innerHTML = "The grade is=C"
+                    }
+
+                    else if(parseFloat(marks)>=33)
+
+                        {
+                             document.getElementById('result').innerHTML = "The grade is=D"
+                        }
+
+                        else (parseFloat(marks)<=30)
+
+                            {
+                                 document.getElementById('result').innerHTML = "The grade is=F"
+                            }
 }
